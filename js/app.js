6f1491963cdad94381888744f260534b465a5908
Камрон <kmirzoev77@gmail.com>
2023-05-13 19:30:25 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-if(ScrollTrigger.isTouch == 1) {
+if(ScrollTrigger.isTouch !==1) {
 	ScrollSmoother.create({
 		wrapper: '.wrapper',
 		content: '.content',
